934fa18d814050c0c39a44c43a56731d9b0d7a59
yunyangge <gyy23233@163.com>
2025-11-13 01:39:33 +0800

    load warning

--- ultrai2v/distributed/checkpoint.py
+++ ultrai2v/distributed/checkpoint.py
@@ -106,75 +106,81 @@ class Checkpointer:
         last_model_checkpoint = (
             f"{self.save_root_dir}/{PREFIX}{self.last_training_iteration:09d}/{model_checkpoint}"
         )
-        print(f'resume last_model_checkpoint from {last_model_checkpoint}')
-        full_sd = torch.load(
-            last_model_checkpoint, mmap=True, weights_only=True, map_location="cpu"
-        )
-        self.load_state_dict(model, full_sd, dcp_api=self.dcp_api)
-        del full_sd
+        if os.path.exists(last_model_checkpoint):
+            print(f'resume last_model_checkpoint from {last_model_checkpoint}')
+            full_sd = torch.load(
+                last_model_checkpoint, mmap=True, weights_only=True, map_location="cpu"
+            )
+            self.load_state_dict(model, full_sd, dcp_api=self.dcp_api)
+            del full_sd
+        else:
+            print(f'warning! nothing find in {last_model_checkpoint}')
 
     def load_optim(self, model: FSDPModule, opt: torch.optim.Optimizer):
         last_optim_checkpoint = (
             f"{self.save_root_dir}/{PREFIX}{self.last_training_iteration:09d}/{OPTIM_CHECKPOINT}"
         )
-        print(f'resume last_optim_checkpoint from {last_optim_checkpoint}')
-        full_sd = torch.load(
-            last_optim_checkpoint, mmap=True, weights_only=True, map_location="cpu"
-        )
-        if self.dcp_api:
-            set_optimizer_state_dict(
-                model=model,
-                optimizers=opt,
-                optim_state_dict=full_sd,
-                options=StateDictOptions(
-                    full_state_dict=True,
-                    broadcast_from_rank0=True,
-                ),
+        if os.path.exists(last_optim_checkpoint):
+            print(f'resume last_optim_checkpoint from {last_optim_checkpoint}')
+            full_sd = torch.load(
+                last_optim_checkpoint, mmap=True, weights_only=True, map_location="cpu"
+            )
+            if self.dcp_api:
+                set_optimizer_state_dict(
+                    model=model,
+                    optimizers=opt,
+                    optim_state_dict=full_sd,
+                    options=StateDictOptions(
+                        full_state_dict=True,
+                        broadcast_from_rank0=True,
+                    ),
+                )
+                del full_sd
+                gc.collect()
+                return
+            _init_optim_state(opt)
+            param_groups = opt.state_dict()["param_groups"]
+            state = opt.state_dict()["state"]
+
+            full_param_groups = full_sd["param_groups"]
+            full_state = full_sd["state"]
+
+            for param_group, full_param_group in zip(param_groups, full_param_groups):
+                for key, value in full_param_group.items():
+                    if key == PARAMS:
+                        continue
+                    param_group[key] = value
+                for pid, full_pid in zip(param_group[PARAMS], full_param_group[PARAMS]):
+                    if pid not in state:
+                        continue
+                    param_state = state[pid]
+                    full_param_state = full_state.get(full_pid, None)
+                    if full_param_state is None:
+                        if torch.distributed.get_rank() == 0:
+                            print(f"WARN: param [{full_pid}] does NOT have param_state")
+                        continue
+                    for attr, full_tensor in full_param_state.items():
+                        sharded_tensor = param_state[attr]
+                        if isinstance(sharded_tensor, DTensor):
+                            # exp_avg is DTensor
+                            param_state[attr] = distribute_tensor(
+                                full_tensor,
+                                sharded_tensor.device_mesh,
+                                sharded_tensor.placements,
+                            )
+                        else:
+                            # step is plain tensor
+                            param_state[attr] = full_tensor
+            opt.load_state_dict(
+                {
+                    "param_groups": param_groups,
+                    "state": state,
+                }
             )
             del full_sd
             gc.collect()
-            return
-        _init_optim_state(opt)
-        param_groups = opt.state_dict()["param_groups"]
-        state = opt.state_dict()["state"]
-
-        full_param_groups = full_sd["param_groups"]
-        full_state = full_sd["state"]
-
-        for param_group, full_param_group in zip(param_groups, full_param_groups):
-            for key, value in full_param_group.items():
-                if key == PARAMS:
-                    continue
-                param_group[key] = value
-            for pid, full_pid in zip(param_group[PARAMS], full_param_group[PARAMS]):
-                if pid not in state:
-                    continue
-                param_state = state[pid]
-                full_param_state = full_state.get(full_pid, None)
-                if full_param_state is None:
-                    if torch.distributed.get_rank() == 0:
-                        print(f"WARN: param [{full_pid}] does NOT have param_state")
-                    continue
-                for attr, full_tensor in full_param_state.items():
-                    sharded_tensor = param_state[attr]
-                    if isinstance(sharded_tensor, DTensor):
-                        # exp_avg is DTensor
-                        param_state[attr] = distribute_tensor(
-                            full_tensor,
-                            sharded_tensor.device_mesh,
-                            sharded_tensor.placements,
-                        )
-                    else:
-                        # step is plain tensor
-                        param_state[attr] = full_tensor
-        opt.load_state_dict(
-            {
-                "param_groups": param_groups,
-                "state": state,
-            }
-        )
-        del full_sd
-        gc.collect()
+        else:
+            print(f'warning! nothing find in {last_optim_checkpoint}')
 
     def _get_full_model_state_dict(self, model: FSDPModule):
         if self.dcp_api:
@@ -243,21 +249,24 @@ class Checkpointer:
         last_rng_checkpoint_dir = (
             f"{self.save_root_dir}/{PREFIX}{self.last_training_iteration:09d}/{RNG_CHECKPOINT_DIR}"
         )
-        print(f'resume rng_state_dict from {last_rng_checkpoint_dir}')
-        if is_npu_available(): # npu will raise serialization error if we use 'load' func from accelerate
-            rng_state_dict = torch.load(f"{last_rng_checkpoint_dir}/rank_{torch.distributed.get_rank():06d}.pkl", weights_only=False, map_location="cpu")
+        if os.path.exists(last_rng_checkpoint_dir):
+            print(f'resume rng_state_dict from {last_rng_checkpoint_dir}')
+            if is_npu_available(): # npu will raise serialization error if we use 'load' func from accelerate
+                rng_state_dict = torch.load(f"{last_rng_checkpoint_dir}/rank_{torch.distributed.get_rank():06d}.pkl", weights_only=False, map_location="cpu")
+            else:
+                rng_state_dict = load(f"{last_rng_checkpoint_dir}/rank_{torch.distributed.get_rank():06d}.pkl", map_location="cpu")
+            random.setstate(rng_state_dict["random_state"])
+            np.random.set_state(rng_state_dict["numpy_random_seed"])
+            torch.set_rng_state(rng_state_dict["torch_manual_seed"])
+            torch.cuda.set_rng_state_all(rng_state_dict["torch_cuda_manual_seed"])
+            if is_npu_available():
+                import torch_npu
+                torch_npu.npu.set_rng_state_all(rng_state_dict["torch_npu_manual_seed_all"])
+                torch_npu.npu.set_rng_state(rng_state_dict["torch_npu_manual_seed"])
+            del rng_state_dict
+            gc.collect()
         else:
-            rng_state_dict = load(f"{last_rng_checkpoint_dir}/rank_{torch.distributed.get_rank():06d}.pkl", map_location="cpu")
-        random.setstate(rng_state_dict["random_state"])
-        np.random.set_state(rng_state_dict["numpy_random_seed"])
-        torch.set_rng_state(rng_state_dict["torch_manual_seed"])
-        torch.cuda.set_rng_state_all(rng_state_dict["torch_cuda_manual_seed"])
-        if is_npu_available():
-            import torch_npu
-            torch_npu.npu.set_rng_state_all(rng_state_dict["torch_npu_manual_seed_all"])
-            torch_npu.npu.set_rng_state(rng_state_dict["torch_npu_manual_seed"])
-        del rng_state_dict
-        gc.collect()
+            print(f'warning! nothing find in {last_rng_checkpoint_dir}')
 
     def _get_full_rng_state_dict(self):
         states = {}
